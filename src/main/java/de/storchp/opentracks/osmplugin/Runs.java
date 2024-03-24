aba18b2e846188843d916b7d3f7540f941df0729
danbin <dan.tranng@gmail.com>
2024-03-24 17:40:05 -0400

    Testing commit

---
+++ src/main/java/de/storchp/opentracks/osmplugin/Runs.java
@@ -0,0 +1,12 @@
+package de.storchp.opentracks.osmplugin;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Runs
+{
+
+}
